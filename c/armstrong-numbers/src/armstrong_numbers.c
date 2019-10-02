f59a16b1045bdba402c9994c8db199cdda6019b7
Z Knight <timeowl@gmail.com>
2019-10-01 23:03:19 -0700

    Refactored c/armstrong numbers

--- c/armstrong-numbers/src/armstrong_numbers.c
+++ c/armstrong-numbers/src/armstrong_numbers.c
@@ -1,12 +1,12 @@
 #include <stdbool.h> 
+// #include <stdio.h>
 #include "armstrong_numbers.h"
 
 /* Name: armstrong_numbers.c
  * Purpose: exercism, c track, armstrong numbers
  * Written by Z Knight, 2019.09.11
+ *   Modified by ZK for smaller memory requirement, 2019.10.01
  */
-
-#define MAXDIGITS 10
 
 int intPow(int base, int exponent)
 // works recursively
@@ -20,27 +20,25 @@ int intPow(int base, int exponent)
 
 bool isArmstrongNumber(int input_number)
 {
-  int digits[MAXDIGITS];
   int my_digit;
   int reduced_number = input_number;
   int partial_sum = 0;
-  int num_digits = 1;
-  // separate input into digits
-  for(int digit_num = 0; digit_num < MAXDIGITS; digit_num++) {
+  int num_digits = 0;
+  
+  // count the digits
+  // printf("Input number: %d\n", input_number);
+  while(reduced_number >= 1) {
+    reduced_number /= 10;
+    num_digits += 1;
+  }
+  // printf("num digits: %d\n", num_digits);
+
+  // separate input into digits and raise to power
+  reduced_number = input_number;
+  while(reduced_number >= 1) {
     my_digit = reduced_number % 10;
     reduced_number /= 10;
-    digits[digit_num] = my_digit;
-    if(my_digit != 0) {
-      num_digits = digit_num+1;
-    }
-  }
-  
-  if(num_digits == 1) {
-    return true;
-  }
-
-  for(int digit_num=0; digit_num < num_digits; digit_num++)  {
-    partial_sum += intPow(digits[digit_num], num_digits);
+    partial_sum += intPow(my_digit, num_digits);
   }
   if(partial_sum == input_number) {
     return true;
