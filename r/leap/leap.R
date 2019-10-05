acd46ea816207cfe6e46f7bbe9e762bee261e297
Z Knight <timeowl@gmail.com>
2019-10-05 12:28:30 -0700

    Modified r leap to use and,or instead of nested loops

--- r/leap/leap.R
+++ r/leap/leap.R
@@ -1,15 +1,10 @@
 # Name: leap.R
 # Purpose: exercism, R track, Leap
 # Written by Z Knight, 2019.10.05
+#  Modified to and and or instead of nested loops; ZK, 2019.10.05
 #
 leap <- function(year) {
-  if (year %% 4 == 0) {
-    if (year %% 100 == 0) {
-      if ( year %% 400 == 0) {
-        return(TRUE)
-      }
-      return(FALSE)
-    }
+  if (year %% 4 == 0 & (year %% 100 != 0 | year %% 400 == 0)) {
     return(TRUE)
   }
   return(FALSE)
