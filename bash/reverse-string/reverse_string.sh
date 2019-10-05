d2198c34b6086f1fdbe7b1340452f9a7e47fc683
Z Knight <timeowl@gmail.com>
2019-10-05 15:59:27 -0700

    added bash reverse_string

---
+++ bash/reverse-string/reverse_string.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Name: reverse_string.sh
+# Purpose: exercism, bash track, reverse_string
+# Written by Z Knight, 2019.10.05
+
+main () {
+  result=""
+  for (( i=0; i<${#1}; i++ )); do
+    # echo "${1:$i:1}"
+    result="${1:$i:1}$result"
+  done
+
+  echo $result
+}
+
+main "$1"
---
+++ bash/reverse-string/reverse_string_test.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+
+@test "an empty string" {
+  #[[ $BATS_RUN_SKIPPED == true  ]] || skip
+  run bash reverse_string.sh ""
+
+  [[ $status -eq 0 ]]
+  [[ $output = "" ]]
+}
+
+@test "a word" {
+  #[[ $BATS_RUN_SKIPPED == true  ]] || skip
+  run bash reverse_string.sh "robot"
+
+  [[ $status -eq 0 ]]
+  [[ $output = "tobor" ]]
+}
+
+@test "a capitalised word" {
+  #[[ $BATS_RUN_SKIPPED == true  ]] || skip
+  run bash reverse_string.sh "Ramen"
+
+  [[ $status -eq 0 ]]
+  [[ $output = "nemaR" ]]
+}
+
+@test "a sentence with punctuation" {
+  #[[ $BATS_RUN_SKIPPED == true  ]] || skip
+  run bash reverse_string.sh "I'm hungry!"
+
+  [[ $status -eq 0 ]]
+  [[ $output = "!yrgnuh m'I" ]]
+}
+
+@test "a palindrome" {
+  #[[ $BATS_RUN_SKIPPED == true  ]] || skip
+  run bash reverse_string.sh "racecar"
+
+  [[ $status -eq 0 ]]
+  [[ $output = "racecar" ]]
+}
+
+@test "an even-sized word" {
+  #[[ $BATS_RUN_SKIPPED == true  ]] || skip
+  run bash reverse_string.sh "drawer"
+
+  [[ $status -eq 0 ]]
+  [[ $output = "reward" ]]
+}
+
