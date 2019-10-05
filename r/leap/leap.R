3cd504dd3eb8fec0f6380fba9a39736423a4a074
Z Knight <timeowl@gmail.com>
2019-10-05 12:31:43 -0700

    modified r leap to use a single line with no if block

--- r/leap/leap.R
+++ r/leap/leap.R
@@ -2,10 +2,8 @@
 # Purpose: exercism, R track, Leap
 # Written by Z Knight, 2019.10.05
 #  Modified to and and or instead of nested loops; ZK, 2019.10.05
+#  Modified to use a single line instead of an if block; ZK, 2019.10.05
 #
 leap <- function(year) {
-  if (year %% 4 == 0 & (year %% 100 != 0 | year %% 400 == 0)) {
-    return(TRUE)
-  }
-  return(FALSE)
+  year %% 4 == 0 & (year %% 100 != 0 | year %% 400 == 0)
 }
