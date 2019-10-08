453171cb759cc6f1273b68f0bb0c2d114efcf121
Z Knight <timeowl@gmail.com>
2019-10-07 18:21:42 -0700

    javascript comment style change

--- javascript/hello-world/hello-world.js
+++ javascript/hello-world/hello-world.js
@@ -1,8 +1,8 @@
-//
-// Name: hello-world.js
-// Purpose: exercism, javascript track, exercise 1
-// by Z Knight, 2019.09.09
-//
+/*
+ * Name: hello-world.js
+ * Purpose: exercism, javascript track, exercise 1
+ * by Z Knight, 2019.09.09
+ */
 
 export const hello = () => {
   return "Hello, World!"
--- javascript/two-fer/two-fer.js
+++ javascript/two-fer/two-fer.js
@@ -1,8 +1,8 @@
-//
-// Name: two-fer.js
-// Purpose: exercism, javascript track, two-fer
-// by Z Knight, 2019.09.10
-//
+/*
+ * Name: two-fer.js
+ * Purpose: exercism, javascript track, two-fer
+ * by Z Knight, 2019.09.10
+ */
 
 export function twoFer(name='you') {
   return "One for " + name + ", one for me."
