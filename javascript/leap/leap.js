acc8406dd377a423c8963ec8cbf2a64041bdd9f5
Z Knight <timeowl@gmail.com>
2020-03-29 19:58:49 -0700

    Modified javascript/leap

--- javascript/leap/leap.js
+++ javascript/leap/leap.js
@@ -2,13 +2,10 @@
  * Name: leap.js
  * Purpose: exercism, javascript track, leap
  * by Z Knight, 2019.10.11
+ *   Simplified to single line; ZK, 2020.03.29
  */
 
 export const isLeap = (year) => {
-  var result = false;
-  if (year %4 == 0 && ( year %100 != 0 || year %400 == 0 )) {
-    return true;
-  }
-  return result;
+  return (year %4 == 0 && ( year %100 != 0 || year %400 == 0 ));
 };
 
