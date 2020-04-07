d6e41d74a13229a13d77a21e830f96536fdc2fdf
Z Knight <timeowl@gmail.com>
2020-04-07 00:26:23 -0700

    Modified javascript/resistor-color and resistor-color-duo

--- javascript/resistor-color/resistor-color.js
+++ javascript/resistor-color/resistor-color.js
@@ -2,17 +2,11 @@
  * Name: resistor-color.js
  * Purpose: exercism, javascript track, resistor-color
  * by Z Knight, 2019.10.07
+ *   Modified to use .indexOf method; ZK, 2020.04.07
  */
 
 export const colorCode = (colorToFind) => {
-  var found_index = -1;
-  COLORS.forEach(function (item, index) {
-    if (item == colorToFind) {
-      console.log(item);
-      found_index = index;
-    }
-  });
-  return found_index;
+  return COLORS.indexOf(colorToFind)
 };
 
 export const COLORS = ["black","brown","red","orange","yellow","green","blue","violet","grey","white"];
