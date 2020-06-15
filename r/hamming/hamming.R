4b0ae69e81ac546d3efcec9aa0dfb5ce1684eb98
Z Knight <timeowl@gmail.com>
2020-06-15 10:50:07 -0700

    Modified r/hamming per mentor's recommendation

--- r/hamming/hamming.R
+++ r/hamming/hamming.R
@@ -2,21 +2,21 @@
 # Purpose: exercism, R track, hamming: 
 #    Calculate the Hamming difference between two DNA strands.
 # Written by Z Knight, 2020.04.10
+#   Modified to use strsplit and multiple n_acid variables; ZK, 2020.06.14
+#   Fixed list of vector indexing problem; ZK, 2020.06.15
 #
 hamming <- function(strand1, strand2) {
     distance <- 0
-    n_acids = nchar(strand1)
-    if (n_acids != nchar(strand2)) {
+    n_acids1 = nchar(strand1)
+    n_acids2 = nchar(strand2)
+    if (n_acids1 != n_acids2) {
         stop("strands of unequal length!")
     }
     
-    for (i in c(1:n_acids)) {
-        acid1 = substring(strand1, i, i)
-        acid2 = substring(strand2, i, i)
-        # print(c(i, acid1, acid2))
-        if (acid1 != acid2) {
-            distance <- distance +1
-        }
-    }
-    return(distance)
+    acids1 = strsplit(strand1, "")
+    acids2 = strsplit(strand2, "")
+    comparison <- acids1[[1]] == acids2[[1]]
+    num_matches = sum(comparison)
+    
+    return(n_acids1-num_matches)
 }
