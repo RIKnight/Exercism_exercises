e77863796c0050709cf7dd298d8541d47bb4dde5
Z Knight <timeowl@gmail.com>
2021-10-13 15:12:03 -0700

    Added javascript/clock

---
+++ javascript/clock/clock.js
@@ -0,0 +1,44 @@
+/*
+ * Name: clock.js
+ * Purpose: exercism, javascript track, clock
+ * by Z Knight, 2021.10.13
+ */
+
+var numeral = require('numeral');
+
+export class Clock {
+  constructor(newHours, newMinutes=0) {
+    // set starting point
+    // note:  % in javascript is a remainder operator, not a modulo operator!
+    this.minutes = 0;
+    this.hours = (newHours % 24 + 24) % 24;
+    // add new minutes
+    this.plus(newMinutes);
+  }
+
+  toString() {
+    return numeral(this.hours).format('00')+":"+numeral(this.minutes).format('00');
+  }
+
+  plus(newMinutes) {
+    // input:  the number of minutes to add
+    // modify inputs
+    var remainingMinutes = ((this.minutes + newMinutes) % 60 + 60) % 60;
+    var extraHours = (this.minutes + newMinutes - remainingMinutes) / 60;
+    // save results
+    this.hours = ((this.hours + extraHours) % 24 + 24) % 24;
+    this.minutes = remainingMinutes;
+
+    return this;
+  }
+
+  minus(newMinutes) {
+    // input:  the number of minutes to subtract
+    return this.plus(-1*newMinutes);
+  }
+
+  equals(newClock) {
+    // input:  a clock to compare to
+    return this.minutes == newClock.minutes && this.hours == newClock.hours;
+  }
+}
