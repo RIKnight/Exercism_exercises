0bb9fe8e84a2f68fac73fbd13b34ed75c817e39e
Z Knight <timeowl@gmail.com>
2021-10-15 08:05:53 -0700

    added support for multiple saddle points in one row

--- python/saddle-points/saddle_points.py
+++ python/saddle-points/saddle_points.py
@@ -7,7 +7,9 @@
         exercism, python track, saddle points: 
             Detect saddle points in a matrix.
     Written by:
-        Z Knight, 2020.09.18
+        Z Knight, 2020.09.18 (started)
+    Modified:
+        Refactored and completed; ZK, 2021.10.14
 """
 def saddle_points(matrix):
     """
@@ -33,20 +35,31 @@ def saddle_points(matrix):
         column_minima_indices.append(0)
     
     for row_index, row in enumerate(matrix):
-        # get the row min, max:
-        row_maxima_indices.append(row.index(max(row)))
-    
+        # create a list to catch multiple row maxima
+        print(f"row: {row}")
+        row_maxima_list = []
+        row_max = max(row)
+        for col_index, col in enumerate(row):
+            if col == row_max:
+                row_maxima_list.append(col_index)
+        row_maxima_indices.append(row_maxima_list)
+        print(f"row maxima list: {row_maxima_list}")
+
         # continue search for column min, max:
         for column_index, value in enumerate(row):
             if value < column_minima[column_index]:
                 column_minima[column_index] = value
                 column_minima_indices[column_index] = row_index
 
+    print(f"column min indices: {column_minima_indices}")
+    print(f"row max indices: {row_maxima_indices}")
     # stitch them together
     # do the column maxima line up with the row minima?
     output_indices_list = []
     for column_index, column_min_row_index in enumerate(column_minima_indices):
-        if row_maxima_indices[column_min_row_index] == column_index:
-            output_indices_list.append([column_index, column_min_row_index])
+        for row_max_index in row_maxima_indices[column_min_row_index]:
+            if row_max_index == column_index:
+                # add one to each to transform to 1-based counting format
+                output_indices_list.append({"row":column_min_row_index+1, "column":column_index+1})
 
     return output_indices_list
