c17ac25e12f551713d6f739cb891c7efa2dd8e07
Z Knight <timeowl@gmail.com>
2021-11-09 10:37:32 -0800

    Removed cpp build files and directories

--- cpp/hamming/CMakeFiles/test_hamming.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/test_hamming"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/test_hamming.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
